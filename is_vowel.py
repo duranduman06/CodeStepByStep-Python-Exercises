0a7731ccfbc62cac223640b31502c7b37d1c6b19
Duran Duman <duranduman06@gmail.com>
2022-09-22 13:01:10 +0300

    Create is_vowel.py

---
+++ is_vowel.py
@@ -0,0 +1,6 @@
+"""
+Write a function named is_vowel that returns whether a string is a vowel (a single-letter string containing a, e, i, o, or u, case-insensitively).
+"""
+
+def is_vowel(letter):
+    return (letter=="a" or letter=="e" or letter=="i" or letter=="o" or letter=="u" or letter=="A" or letter=="E" or letter=="I" or letter=="O" or letter=="U")
