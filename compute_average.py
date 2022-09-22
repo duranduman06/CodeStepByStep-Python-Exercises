24bd37f715b89ff853d960ac0750fb0fbd655eb1
Duran Duman <duranduman06@gmail.com>
2022-09-22 13:54:45 +0300

    Create compute_average.py

---
+++ compute_average.py
@@ -0,0 +1,13 @@
+"""
+Write a function named compute_average that computes and returns the mean of all elements in a list of integers.
+For example, if a list named a contains [1, -2, 4, -4, 9, -6, 16, -8, 25, -10], then the call of compute_average(a) should return 2.5.
+
+Constraints: You may assume that the list contains at least one element. Your function should not modify the elements of the list.
+"""
+
+def compute_average(a):
+    sum=0
+    for i in range(0,len(a)):
+        sum+=a[i]
+    average=sum/len(a)
+    return average
