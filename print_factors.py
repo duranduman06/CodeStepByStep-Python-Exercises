f0b84a6ca121836a28d3fed8f936a7653bf00044
Duran Duman <duranduman06@gmail.com>
2022-09-22 13:37:44 +0300

    Update print_factors.py

--- print_factors.py
+++ print_factors.py
@@ -5,6 +5,7 @@ separated by the word " and ". For example, the number 24's factors should print
 1 and 2 and 3 and 4 and 6 and 8 and 12 and 24
 You may assume that the number parameter's value is greater than 0.
 """
+
 def print_factors(num):
     for i in range(1,num+1):
         if(num==i):
