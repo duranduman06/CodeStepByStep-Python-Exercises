c71e282119cc4093b7fb90802c2b7e3e1be3751a
Duran Duman <duranduman06@gmail.com>
2022-09-22 13:52:29 +0300

    Create collapse.py

---
+++ collapse.py
@@ -0,0 +1,17 @@
+"""
+Write a function named collapse that accepts a list of integers as a parameter and returns a new list where each pair of integers from the original list has been replaced by the sum of that pair.
+For example, if a list called a stores [7, 2, 8, 9, 4, 13, 7, 1, 9, 10], then the call of collapse(a) should return a new list containing [9, 17, 17, 8, 19]. 
+The first pair from the original list is collapsed into 9 (7 + 2), the second pair is collapsed into 17 (8 + 9), and so on.
+
+If the list stores an odd number of elements, the element is not collapsed. For example, if the list had been [1, 2, 3, 4, 5], then the call would return [3, 7, 5]. 
+Your function should not change the list that is passed as a parameter.
+"""
+def collapse(list):
+    emptylist=[]
+    i=0
+    while i<len(list)-1:
+        emptylist.append(list[i]+list[i+1])
+        i+=2
+    if(len(list)%2!=0):
+        emptylist.append(list[-1])
+    return emptylist
