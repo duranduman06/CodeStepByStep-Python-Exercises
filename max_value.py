618c54e56d27720545ad052b9343d647a792aa47
Duran Duman <duranduman06@gmail.com>
2022-09-22 14:04:28 +0300

    Create max_value.py

---
+++ max_value.py
@@ -0,0 +1,7 @@
+"""
+Write a function named max_value that accepts a list of integers as a parameter and returns the maximum value in the list. 
+For example, if a list named nums passed stores [17, 7, -1, 26, 3, 9], the call of max_value(nums) should return 26.
+You may assume that the list contains at least one element. Your function should not modify the elements of the list.
+"""
+def max_value(nums):
+        return max(nums)
