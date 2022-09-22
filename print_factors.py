14127d033dbfbcc989a551e82a08ae6eedb06ee3
Duran Duman <duranduman06@gmail.com>
2022-09-22 13:37:13 +0300

    Update print_factors.py

--- print_factors.py
+++ print_factors.py
@@ -1,6 +1,6 @@
 """
-Write a function named print_factors that accepts an integer as its parameter and uses a fencepost loop to print the factors of that number, separated by the word " and ". 
-For example, the number 24's factors should print as:
+Write a function named print_factors that accepts an integer as its parameter and uses a fencepost loop to print the factors of that number,
+separated by the word " and ". For example, the number 24's factors should print as:
 
 1 and 2 and 3 and 4 and 6 and 8 and 12 and 24
 You may assume that the number parameter's value is greater than 0.
